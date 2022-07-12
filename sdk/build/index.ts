5c542bc141e25b790162b782047d2fe71b26a402
lmvdz <xeonpowder@gmail.com>
2022-07-11 23:49:03 -0500

    latest

--- sdk/build/index.ts
+++ sdk/build/index.ts
@@ -7,7 +7,7 @@ const isWin = process.platform === "win32";
 if (isWin) {
     exec('(if exist lib ( rmdir /s /q lib )) && tsc');
 } else {
-    exec('(if exist ../target ( rm -rf ./src/idl/* && cp ../target/idl/*.json ./src/idl/ ))')
-    exec('(if exist ../target ( rm -rf ./src/types/* && cp ../target/types/*.ts ./src/types/ ))')
+    exec('[ -d "../target" ] && ( rm -rf ./src/idl/* && cp ../target/idl/*.json ./src/idl/ )')
+    exec('[ -d "../target" ] && ( rm -rf ./src/types/* && cp ../target/types/*.ts ./src/types/ )')
     exec('rm -rf lib && tsc');
 }
